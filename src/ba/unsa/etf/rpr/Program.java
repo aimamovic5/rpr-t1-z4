e5ceb135c750b1e255929754b68f8a885c9ec63a
aimamovic5 <44173127+aimamovic5@users.noreply.github.com>
2018-10-28 00:30:40 +0200

    dodao metode obrisiStudenta i obrisiPredmet

--- src/ba/unsa/etf/rpr/Program.java
+++ src/ba/unsa/etf/rpr/Program.java
@@ -4,7 +4,7 @@ public class Program {
     private Student[] studenti;
     private Predmet[] predmeti;
 // potrebno: kreiranje novog predmeta, novog studenta, upis studenata na predmet, ispis
-    // studenata sa predmeta, brisanje studenta, brisanej predmeta, ispis spiska studenata
+    // studenata sa predmeta, brisanje studenta, brisanje predmeta, ispis spiska studenata
     // na predmetu
     public void dodajPredmet(String nazivPredmeta, Integer sifraPredmeta, Integer maksimalanBrojStudenata) {
         Predmet[] pomocni = new Predmet[this.predmeti.length + 1];
@@ -17,6 +17,22 @@ public class Program {
         Student[] pomocni = new Student[this.studenti.length + 1];
         pomocni[this.studenti.length] = new Student(imeStudenta, prezimeStudenta, brojIndexa);
         this.studenti = pomocni;
+    }
+    public void obrisiStudenta(Student student) {
+        int pronadjen = 0;
+        Student[] temp = new Student[this.studenti.length - 1];
+        for (int i = 0; i < studenti.length; i++)
+            if (this.studenti[i].getBrojIndeksa() != student.getBrojIndeksa()) temp[i-pronadjen] = this.studenti[i];
+            else pronadjen = 1;
+        this.studenti = temp;
+    }
+    public void obrisiPredmet(Predmet predmet) {
+        int pronadjen = 0;
+        Predmet[] temp = new Predmet[this.predmeti.length - 1];
+        for (int i = 0; i < predmeti.length; i++)
+            if (this.predmeti[i].getSifraPredmeta() != predmet.getSifraPredmeta()) temp[i-pronadjen] = this.predmeti[i];
+            else pronadjen = 1;
+        this.predmeti = temp;
     }
     public static void main(String[] args) {
 	// write your code here
